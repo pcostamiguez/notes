b530804f2aee2da1e47efa4e7a34a701d68715c6
Pedro Henrique Costa Miguez <pcostamiguez@gmail.com>
2024-09-15 13:55:06 -0300

    Auth

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt_BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @vite('resources/css/pace-theme-flat-top.css')
+    @vite('resources/js/pace.min.js')
+
+    @vite('resources/css/app.css')
+    @vite('resources/js/app.js')
+
+    @vite('resources/css/bootstrap.min.css')
+    @vite('resources/css/all.min.css')
+
+    @vite('resources/js/bootstrap.bundle.min.js')
+    @vite('resources/js/all.min.js')
+
+    <title>Notes</title>
+</head>
+<body>
+@yield('content')
+</body>
+</html>
